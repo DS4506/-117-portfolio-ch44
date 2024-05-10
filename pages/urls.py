b44c0ac2578531d830950c65839c65ea5192b7f3
DS4506 <davidsmith4506@gmail.com>
2024-05-09 22:10:09 -0500

    newnew

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import test_view
+from .views import test_view, about_view
 
 urlpatterns = [
     path('testing', test_view, name="test"),
+    path("", about_view, name="about"),
+    
 ]
